62c69da51cf933b4c3cec7e0448038f6d84014d0
Zeth <zeth.leonardo@protonmail.com>
2020-05-15 03:53:27 +0400

    Remove TODO comment.

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,6 @@ app.get('/', (req: Request, res: Response) => {
   res.send("Hello World!");
 });
 
-// TODO: Get port from environment variables.
 const port: number = Number(process.env.PORT) || 5000;
 
 // Run the application
