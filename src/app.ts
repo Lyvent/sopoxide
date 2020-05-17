6b481c2ad73784807f3aeaba562b6c9b1c5a964d
Zeth <zeth.leonardo@protonmail.com>
2020-05-17 21:25:50 +0400

    Add Mongoose connection.

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
 import express, { Application, Request, Response } from 'express';
+import mongoose, { ConnectionOptions } from 'mongoose';
 
 const app: Application = express();
 
-app.get('/', (req: Request, res: Response) => {
-  res.send("Hello World!");
+// Connect to MongoDB
+const URI: string = 'mongodb://localhost/lyventdb';
+const DBOptions: ConnectionOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(URI, DBOptions, () => {
+  console.log('Connected to DB');
 });
 
+// Run the application
 const port: number = Number(process.env.PORT) || 5000;
 
-// Run the application
 app.listen(port, () => {
   console.log(`Server is running on :${port}`);
 });
