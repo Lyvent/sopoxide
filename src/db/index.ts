79ecaca8aff3364ebe42a681787bd1585a31e138
Zeth <zeth.leonardo@protonmail.com>
2020-05-17 21:41:43 +0400

    Added Database module.

---
+++ src/db/index.ts
@@ -0,0 +1,19 @@
+import mongoose, { ConnectionOptions } from 'mongoose';
+
+const URI: string = 'mongodb://localhost/lyventdb';
+const DBOptions: ConnectionOptions = {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+};
+
+mongoose.connect(URI, DBOptions);
+
+const db = mongoose.connection;
+
+// Handle connection events
+db.on('error', console.error.bind(console, 'Connection Error: '));
+db.once('open', () => {
+  console.log('Connected to DB.');
+});
+
+export default db;
