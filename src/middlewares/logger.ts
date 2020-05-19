59b71723ac6018afddb2a4cbedc3ecc96a912faf
Zeth <zeth.leonardo@protonmail.com>
2020-05-19 17:42:29 +0400

    Added winston logger.

--- src/middlewares/logger.ts
+++ src/middlewares/logger.ts
@@ -1,1 +1,29 @@
-// TODO: Add options for better logging.
+import { createLogger, transports, format } from 'winston';
+
+const logger = createLogger({
+  transports: [
+    new transports.File({
+      level: 'info',
+      filename: '../../logs/app.log',
+      handleExceptions: true,
+      maxFiles: 5,
+      maxsize: 5242880, // 5MB
+    }),
+
+    new transports.Console({
+      level: 'debug',
+      handleExceptions: true,
+    })
+  ],
+
+  exitOnError: false
+});
+
+// Log to console if not in prod. Check winston docs.
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(new transports.Console({
+    format: format.simple(),
+  }));
+}
+
+export default logger;
