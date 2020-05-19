dda2d697c604c91e67a1cda31c3672c22851c709
Zeth <zeth.leonardo@protonmail.com>
2020-05-19 14:14:33 +0400

    Added more middleware.

--- src/app.ts
+++ src/app.ts
@@ -2,25 +2,32 @@ import express, { Application } from 'express';
 import 'dotenv/config';
 
 // Middleware
-import bodyParser from 'body-parser';
-import corsMiddleware from './middleware/cors';
+import helmet from 'helmet';
+import morgan from 'morgan';
+
+import cors from 'cors';
+import corsOptions from './middlewares/corsOptions';
 
 import connectDB from './db/index';
 
 const app: Application = express();
 
 // Instantiate Middlewares
-app.use(bodyParser.json());
-app.use(corsMiddleware);
+app.use(express.json());
+app.use(helmet());
+app.use(cors(corsOptions));
+app.use(morgan('combined'));
 
 // Create DB connection.
 connectDB();
 
 // Import Routes
-// TODO: Add Route middleware that is imported for this module (app.ts).
-import { storyRoutes } from './routes/story';
+// TODO: Add route index that can be imported once for all routes.
+import { storyRoutes } from './routes/storyRoute';
+import { authRoutes } from './routes/authRoute';
 
 app.use('/story', storyRoutes);
+app.use('/auth', authRoutes);
 
 // Run the application
 const port: number = Number(process.env.PORT) || 5000;
@@ -28,3 +35,6 @@ const port: number = Number(process.env.PORT) || 5000;
 app.listen(port, () => {
   console.log(`Server is running on :${port}`);
 });
+
+// Export app for testing.
+export default app;
