428d247b4ee179675b9ac1b625709734410a53e4
Zeth <zeth.leonardo@protonmail.com>
2020-05-20 04:38:41 +0400

    Use route index and winston logger.

--- src/app.ts
+++ src/app.ts
@@ -1,39 +1,41 @@
 import express, { Application } from 'express';
 import 'dotenv/config';
 
-// Middleware
+import connectDB from './db/index';
+import { routeIndex } from './routes/index';
+
+// Middleware & Utilities
 import helmet from 'helmet';
 import morgan from 'morgan';
+import logger from './middleware/logger';
 
 import cors from 'cors';
-import corsOptions from './middlewares/corsOptions';
+import corsOptions from './middleware/corsOptions';
 
-import connectDB from './db/index';
+import passport from './middleware/auth';
+// 
 
 const app: Application = express();
 
-// Instantiate Middlewares
+// Instantiate middlewares
 app.use(express.json());
 app.use(helmet());
 app.use(cors(corsOptions));
 app.use(morgan('combined'));
 
+app.use(passport.initialize());
+
 // Create DB connection.
 connectDB();
 
-// Import Routes
-// TODO: Add route index that can be imported once for all routes.
-import { storyRoutes } from './routes/storyRoute';
-import { authRoutes } from './routes/authRoute';
-
-app.use('/story', storyRoutes);
-app.use('/auth', authRoutes);
+// Load imported index routes.
+app.use(routeIndex);
 
 // Run the application
 const port: number = Number(process.env.PORT) || 5000;
 
 app.listen(port, () => {
-  console.log(`Server is running on :${port}`);
+  logger.log('info', `Server is running on :${port}`);
 });
 
 // Export app for testing.
