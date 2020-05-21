7a4990e70646083dbe3863cb821ac4ba86b06da7
Zeth <zeth.leonardo@protonmail.com>
2020-05-22 02:59:37 +0400

    Added response helper.

---
+++ src/helpers/response.ts
@@ -0,0 +1,18 @@
+// INFO: Helper for returning responses.
+import { Response } from 'express';
+
+const badRequestResponse = (res: Response, message: string) => {
+  res.status(400).json({
+      message: message,
+      error: 'data_404'
+  })
+}
+
+const serverErrResponse = (res: Response) => {
+  res.status(500).json({
+    message: 'An error occured!',
+    error: 'server_error'
+  });
+}
+
+export { badRequestResponse, serverErrResponse };
