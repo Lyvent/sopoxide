f0ddf7085489186eba813fa94c140142fd872453
Zeth <zeth.leonardo@protonmail.com>
2020-05-25 00:46:24 +0400

    Small additions to models.

--- src/models/Story.ts
+++ src/models/Story.ts
@@ -3,7 +3,20 @@ import mongoose, { Schema } from 'mongoose';
 const StorySchema = new Schema({
   title: {
     type: String,
+    maxlength: 100,
     required: true,
+  },
+
+  content: {
+    type: String,
+    required: true
+  },
+
+  // Author Reference.
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
   },
 
   createdAt: {
--- src/models/Thought.ts
+++ src/models/Thought.ts
@@ -1,1 +1,10 @@
-// TODO: Add story thought (comment/rating) features.
+import mongoose, { Schema } from 'mongoose';
+
+const ThoughtSchema = new Schema({
+  // TODO: Fill this out.
+});
+
+const Thought = mongoose.model('Thought', ThoughtSchema);
+
+export default Thought;
+export { ThoughtSchema };
