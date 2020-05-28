39d7621e1f24f785d3ea8669d4c47e56b1664a14
Zeth <zeth.leonardo@protonmail.com>
2020-05-29 01:08:08 +0400

    Added rest of CRUD routes w/Handlers.

--- src/routes/storyRouter.ts
+++ src/routes/storyRouter.ts
@@ -1,17 +1,32 @@
 import passport from 'passport';
 import { Router } from 'express';
 import {
+  createStory,
   getStory,
+  updateStory,
   deleteStory
 } from '../handlers/storyHandler';
 
 const router = Router();
 
 router.get('/:storyID', getStory);
+
+router.post(
+  '/create',
+  passport.authenticate('jwt', { session: false }),
+  createStory
+);
+
+router.put(
+  '/update',
+  passport.authenticate('jwt', { session: false }),
+  updateStory
+);
+
 router.delete(
   '/:storyID',
   passport.authenticate('jwt', { session: false }),
   deleteStory
-)
+);
 
 export { router as storyRoutes }; 
