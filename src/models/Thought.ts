74f300b9ff5bdef3e1785ecc624a37d4120450e2
Zeth <zeth.leonardo@protonmail.com>
2020-06-21 04:03:54 +0400

    Added basic thought fields.

--- src/models/Thought.ts
+++ src/models/Thought.ts
@@ -2,6 +2,17 @@ import mongoose, { Schema } from 'mongoose';
 
 const ThoughtSchema = new Schema({
   // TODO: Fill this out.
+  // Author Reference.
+  author: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+
+  message: {
+    type: String,
+    required: true
+  }
 });
 
 const Thought = mongoose.model('Thought', ThoughtSchema);
