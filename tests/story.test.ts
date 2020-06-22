6d63bf0603ee6aac29c02b9e323d65016bdf1fb7
Zeth <zeth.leonardo@protonmail.com>
2020-06-23 02:32:43 +0400

    Added base story unit test.

---
+++ tests/story.test.ts
@@ -0,0 +1,9 @@
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+
+import Story from '../src/models/Story';
+
+// Get all stories
+describe('Get all stories', () => {
+  // @TODO: Implement.
+});
