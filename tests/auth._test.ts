67dfe3a7ae47ff03dd9171a9188b769aaed94c1f
Zeth <zeth.leonardo@protonmail.com>
2020-06-24 22:03:28 +0400

    Going to be refactored.

--- tests/auth._test.ts
+++ tests/auth._test.ts
@@ -1,23 +1,2 @@
-// @TODO: Refactor
-import chai, { expect } from 'chai';
-import chaiHttp from 'chai-http';
-import sinon from 'sinon';
 
-import { mockRequest, mockResponse } from 'mock-req-res';
-import AuthHandler from '../src/handlers/authHandler';
-
-const handler = new AuthHandler();
-
-describe('login', () => {
-  it('should 400 if email is missing from the body', async (done) => {
-    const req = mockRequest({
-      body: { password: 'test1234' }
-    });
-    const res = mockResponse();
-
-    await handler.logIn(req, res);
-    
-    done();
-
-  }).timeout(10000);
-});
+// @TODO: Implement auth unit tests w/Ava
