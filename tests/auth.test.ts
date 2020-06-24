40620e4b1113e78d2789bf1e4b0eaabfab8ddbd8
Zeth <zeth.leonardo@protonmail.com>
2020-06-25 03:25:54 +0400

    To be implemented.

--- tests/auth.test.ts
+++ tests/auth.test.ts
@@ -1,9 +1,6 @@
 import test from "ava";
 
-import AuthHandler from '../src/handlers/authHandler';
-
-// const handler = new AuthHandler();
-
-test('login > should 400 if email is missing from the body', t => {
-  // @TODO: Implement login with empty data.
+// @TODO: Implement authentication unit tests.
+test('To be implemented', t => {
+  t.pass();
 });
--- tests/story.test.ts
+++ tests/story.test.ts
@@ -1,1 +1,6 @@
+import test from "ava";
+
 // @TODO: Implement story unit tests w/Ava
+test('To be implemented', t => {
+  t.pass();
+});
