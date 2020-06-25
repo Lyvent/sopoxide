272f5d750273df357ee23d08493331b4fb860501
Zeth <zeth.leonardo@protonmail.com>
2020-06-25 18:26:17 +0400

    Moved into separate folders.

--- tests/auth.test.ts
+++
@@ -1,6 +0,0 @@
-import test from "ava";
-
-// @TODO: Implement authentication unit tests.
-test('To be implemented', t => {
-  t.pass();
-});
--- tests/story.test.ts
+++
@@ -1,6 +0,0 @@
-import test from "ava";
-
-// @TODO: Implement story unit tests w/Ava
-test('To be implemented', t => {
-  t.pass();
-});
