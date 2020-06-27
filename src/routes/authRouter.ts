e8d21b0100db1cf27441203d3f42dcb0e8fca938
Zeth <zeth.leonardo@protonmail.com>
2020-06-28 01:36:43 +0400

    Added limiters.

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,11 +1,17 @@
 import { Router } from 'express';
 import AuthHandler from '../handlers/authHandler';
 
+import {
+  logInLimiter,
+  signUpLimiter
+} 
+from '../middleware/rateLimiting';
+
 const router = Router();
 const handler = new AuthHandler();
 
-router.post('/login', handler.logIn);
-router.post('/signup', handler.signUp);
+router.post('/login', logInLimiter, handler.logIn);
+router.post('/signup', signUpLimiter, handler.signUp);
 /* TEMPORARILY UNAVAILABLE.
 router.post('/google', handler.google);
 */
